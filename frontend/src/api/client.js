8c03b1887454007ad03a3d417116d2f95e56388d
Noman Wahdat <nomanwahdat@gmail.com>
2026-05-10 00:46:20 +0500

    Fix

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: "https://ai-resume-analyzer-production-ca21.up.railway.app/api",
   timeout: 30000,
 })
 
